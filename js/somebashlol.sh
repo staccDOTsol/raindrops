cf307d6edb33f0b59943e75adc92a4478dc672db
jarett dunn <jarettdunn@jaretts-MacBook-Air.local>
2022-08-06 15:17:01 -0300

    la

--- js/somebashlol.sh
+++ js/somebashlol.sh
@@ -3,20 +3,18 @@ nohup ts-node cli/src/matches.ts create_or_update_oracle -cp cli/example-configs
 
 nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k aaa.json -l debug &
 nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug &
-nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/idhydra.json -l debug &
-nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k aaa.json -l debug &
-nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug &
-nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/idhydra.json -l debug &
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/idhydra.json -l debug
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k aaa.json -l debug
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/idhydra.json -l debug
 
 while :
 do 
-ts-node cli/src/matches.ts show_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k aaa.json -l debug &
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug &
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/idhydra.json -l debug
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k aaa.json -l debug
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug
+nohup ts-node cli/src/matches.ts join_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/idhydra.json -l debug
 
-ts-node cli/src/matches.ts join -cp cli/example-configs/match/createMatch.json -e devnet -k aaa.json -l debug
-ts-node cli/src/matches.ts show_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug
-
-ts-node cli/src/matches.ts join -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug
-ts-node cli/src/matches.ts show_match -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/id.json -l debug
-
-ts-node cli/src/matches.ts join -cp cli/example-configs/match/createMatch.json -e devnet -k ~/.config/solana/idhydra.json -l debug
 done 
